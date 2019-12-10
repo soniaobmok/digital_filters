fabe50f0c5001df1298434baa1b17a45c063e5ed
Sofia Obmok <31816016+soniaobmok@users.noreply.github.com>
2019-12-10 18:33:33 +0200

    Update generate.h

--- optimal_filters/optimal_filters/generate.h
+++ optimal_filters/optimal_filters/generate.h
@@ -11,14 +11,14 @@ void generate_line(double* input, int size)
 		input[i] = 1. + ((rand() % 100 / 100. - 0.5) / sqrt(12)) / 4;
 }
 
-void generate_sin_dis(double* input, int size) // дисперсия
+void generate_sin_dis(double* input, int size)
 {
 	srand(0);
 	for (int i = 0; i < size; i++)
 		input[i] = 0.3 * sin(0.1 * i) + ((rand() % 100 / 100. - 0.5) / sqrt(12)) / 4;
 }
 
-void generate_sin_err(double* input, int size)   // погрешность АЦП
+void generate_sin_err(double* input, int size)
 {
 	srand(0);
 	for (int i = 0; i < size; i++)
